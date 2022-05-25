78253682593cdd1a0a76584f6533d2e974a1cb3c
eliashenri <elias.henrique.andrioli@gmail.com>
2022-05-24 23:41:59 -0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Book } from './book.model';
+import { BooksController } from './books.cotroller';
+import { BooksService } from './books.service';
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '88017620',
+      database: 'library',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Book])
+  ],
+  controllers: [AppController, BooksController],
+  providers: [AppService, BooksService],
+})
+export class AppModule {}
---
+++ src/books.cotroller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Book } from './book.model'
+import { BooksService } from "./books.service";
+
+
+@Controller('books')
+export class BooksController {
+    constructor(private booksService: BooksService) {
+
+    }
+
+    @Get()
+    async getAll(): Promise <Book[]> {
+        return this.booksService.getAll();
+    }
+
+    @Get(':id')
+    async getOne(@Param() params): Promise<Book> {
+        return this.booksService.getOne(params.id);
+    }
+
+    @Post()
+    async createBook(@Body() book: Book) {
+        this.booksService.createBook(book);
+    }
+
+    @Put()
+    async modifyBook(@Body() book: Book): Promise<[number, Book[]]> {
+        return this.booksService.modifyBook(book)
+    }
+
+    @Delete(':id')
+    async deleteBook(@Param() params) {
+        this.booksService.deleteBook(params.id);
+    }
+}
